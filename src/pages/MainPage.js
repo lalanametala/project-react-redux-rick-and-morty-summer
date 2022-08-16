05fbe28b5a279d722daa996fab19dce30f82e92d
Lalá Nametala <lalanametala@gmail.com>
2022-08-16 17:45:31 -0300

    :recycle: refactor(MainPage): turning page into function

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import Cards from '../components/Cards';
 import Header from '../components/Header';
-import { fetchCharacters } from '../redux/actions';
 
-class Mainpage extends React.Component {
-  componentDidMount = () => {
-    const { fetchCharactersDispatch } = this.props;
-    fetchCharactersDispatch();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Cards />
-      </div>
-    );
-  }
+function Mainpage() {
+  return (
+    <div>
+      <Header />
+      <Cards />
+    </div>
+  );
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  fetchCharactersDispatch: () => dispatch(fetchCharacters()),
-});
-
-Mainpage.propTypes = {
-  fetchCharactersDispatch: PropTypes.func.isRequired,
-};
-
-export default connect(null, mapDispatchToProps)(Mainpage);
+export default Mainpage;
