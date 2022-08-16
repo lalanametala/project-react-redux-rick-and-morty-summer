9eac52aa2eeb897ea8cb8838a4e72f1882daec1f
Lalá Nametala <lalanametala@gmail.com>
2022-08-16 08:56:47 -0300

    :sparkles: feat(ContextAPI): creating provider file

---
+++ src/context/AppProvider.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import fetchAPI from '../services/data';
+import AppContext from './AppContext';
+
+function AppProvider({ children }) {
+  const [user, setUser] = useState({ username: '', redirect: false });
+  const [characters, setCharacters] = useState([]);
+
+  const fetchCharacters = async () => {
+    const newCharacters = await fetchAPI();
+    setCharacters(newCharacters);
+  };
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={ {
+        user,
+        setUser,
+        characters,
+      } }
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+AppProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppProvider;
